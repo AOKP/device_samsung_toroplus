ff1ce87e8c6780bbbc27e8cbfe4b2447f32ff358
Jean-Baptiste Queru <jbq@google.com>
2012-01-23 15:36:40 -0800

    Rename full_toro_spr to full_torospr.
    The build configuration name must match the device name
    
    Bug: 5827352
    Change-Id: I499d2adb5ab77cb8171c5a09b9159b1a0f03526e

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_toro_spr.mk
+    $(LOCAL_DIR)/full_torospr.mk
--- full_torospr.mk
+++ full_torospr.mk
@@ -40,7 +40,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/torospr/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_toro_spr
+PRODUCT_NAME := full_torospr
 PRODUCT_DEVICE := torospr
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Full AOSP on Toro-SPR
