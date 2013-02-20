c702dfc289a5a6d6e663b9d515eda728fcf743ec
Jean-Baptiste Queru <jbq@google.com>
2013-02-20 08:54:44 -0800

    New date code
    Change-Id: Ieee7e81ac074cf40a90c19266a9ec38a778ec0d1

--- self-extractors/generate-packages.sh
+++ self-extractors/generate-packages.sh
@@ -47,7 +47,7 @@ then
 fi # jb-dev
 (cd ../../../../out/target/product/toroplus ; zip -r ../../../../device/samsung/toroplus/self-extractors/full_toroplus-pseudo_ota.zip system)
 ZIP=full_toroplus-pseudo_ota.zip
-BUILD=20121228
+BUILD=20130220
 ROOTDEVICE=toroplus
 DEVICE=toroplus
 MANUFACTURER=samsung
